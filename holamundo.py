965d6a4d2ec435e5624b8e5c3c6d8e4bd56d0744
JAJUABA <jajuarba@gmail.com>
2022-08-08 19:56:33 +0200

    Create holamundo.py

---
+++ holamundo.py
@@ -0,0 +1,23 @@
+from unittest import result
+from xml.etree.ElementTree import PI
+
+PI = 3.14159
+
+nombre = "José"
+print("Hola Mundo Bootcamp")
+print(nombre)
+
+edad = input("Ingresa tu edad")
+print(edad)
+
+
+x = (input("ingresa primer valor:"))
+y = (input("ingresar segundo valor:"))
+print(float(x) + float(y))
+
+x = (input("ingresa primer valor:"))
+y = (input("ingresar segundo valor:"))
+
+result = int(x) + int(y)
+print(int(PI))
+print("el resultado es " + str(result))
